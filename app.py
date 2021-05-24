ed4c2a47590ffb88f8215c42e5b70b7b2bb053e6
Tom Garrity <tomgarrity2020@gmail.com>
2021-05-24 17:36:46 -0400

    update app.py

--- app.py
+++ app.py
@@ -53,41 +53,74 @@ def precipitation():
     session.close()
     return jsonify(dict_prcp)
 
-# # Return a JSON list of stations from the dataset.
-# @app.route("/api/v1.0/stations")
-# def stations():
-#     session = Session(engine)
-#     stations = session.query(Station.station).all()
-#     session.close()
-#     return jsonify(stations)
+# Return a JSON list of stations from the dataset.
+@app.route("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
+    stations = session.query(Station.station).all()
+    session.close()
+    return jsonify(stations)
 
 # # Query for the dates and temperature observations from a year from the last data point.
-# @app.route("/api/v1.0/tobs")
-# def tobs():
-#     session = Session(engine)
-#     last_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
-#     last_date = last_date[0]
+@app.route("/api/v1.0/tobs")
+def tobs():
+    session = Session(engine)
+    last_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+    last_date = last_date[0]
 
-#     year, month, day = map(int, last_date.split("-"))
-#     year_ago = datetime(year, month, day) - timedelta(days=365)
-#     year_ago = (year_ago.strftime("%Y-%m-%d"))
+    year, month, day = map(int, last_date.split("-"))
+    year_ago = datetime(year, month, day) - timedelta(days=365)
+    year_ago = (year_ago.strftime("%Y-%m-%d"))
 
-#     # tobs = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= "2016-08-23").all()
-#     tobs = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= year_ago).all()
-#     session.close()
-#     return jsonify(tobs)
-#     # return jsonify(last_date)    
-
-
-# # When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date.
-# @app.route("/api/v1.0/<start>")
-# def start_date(start):
-#     list = []
-#     session = Session(engine)
-#     temperature = session.query(Measurement.date,func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
-#                   filter(Measurement.date >= start).group_by(Measurement.date).all()
+    tobs = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= "2016-08-23").all()
+    tobs = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= year_ago).all()
+    session.close()
+    return jsonify(tobs)
+    return jsonify(last_date)    
 
 
+# When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date.
+@app.route("/api/v1.0/<start>")
+def start_date(start):
+    list = []
+    session = Session(engine)
+    temperature = session.query(Measurement.date,func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+                  filter(Measurement.date >= start).group_by(Measurement.date).all()
+
+session.close()
+    for data in temperature:
+        dict = {}
+        dict['Date'] = data[0]
+        dict['Tmin'] = data[1]
+        dict['Tavg'] = round(data[2],2)
+        dict['Tmax'] = data[3]
+        list.append(dict)
+
+    #Return a JSON list
+    return jsonify(list)
+
+@app.route("/api/v1.0/<start>/<end>")
+def start_end_date(start, end):
+    session = Session(engine)
+
+    # Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
+    # When given the start and the end date,  the TMIN, TAVG, and TMAX for dates between the start and end date inclusive.
+    list = []
+    session = Session(engine)
+    temperature = session.query(Measurement.date,func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+                  filter(Measurement.date >= start).filter(Measurement.date <= end).group_by(Measurement.date).all()
+
+    session.close()
+    for data in temperature:
+        dict = {}
+        dict['Date'] = data[0]
+        dict['Tmin'] = data[1]
+        dict['Tavg'] = round(data[2],2)
+        dict['Tmax'] = data[3]
+        list.append(dict)
+
+    #Return a JSON list
+    return jsonify(list)    
 
 if __name__ == '__main__':
     app.run(debug=True)
